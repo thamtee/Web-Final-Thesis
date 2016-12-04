77b1a8ebecd712d7ab4eb5f37a6eb4add6687534
thamtee <thamt56@email.nu.ac.th>
2016-12-04 21:56:56 +0700

    Added a Procfile

--- construction/settings.py
+++ construction/settings.py
@@ -30,6 +30,7 @@ https: // docs.djangoproject.com / en / 1.10 / ref / settings /
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     
     'rest_framework',
     # 'django_stormpath',
@@ -65,41 +67,14 @@ INSTALLED_APPS = [
     'oldConstruction',
 ]
 
-# Stormpath Setting
-# AUTHENTICATION_BACKENDS = (
-#     'django_stormpath.backends.StormpathBackend',    
-#     'django_stormpath.backends.StormpathIdSiteBackend',
-#     'django_stormpath.backends.Stormpath.SocialBackend',
-# )
-
-# # AUTH_USER_MODEL = 'django_stormpath.StormpathUser'
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-#     ]
-# }
-
-# STORMPATH_ID = '6GTHPSFNUBSADHYYEECCE3OQX'
-# STORMPATH_SECRET = 'qN+it57grKYMnQtNotumvofvnv+pk28VCsR5hUb5rRo'
-# STORMPATH_APPLICATION = 'https://api.stormpath.com/v1/applications/3Fs7hhRZf62m6xjFy80K1e'
-
-# STORMPATH_ENABLE_FACEBOOK = True
-
-# STORMPATH_SOCIAL = {
-#     'FACEBOOK': {
-#         'client_id': os.environ['1346565105353812'],
-#         'client_secret': os.environ['222c1b18e8fb79b77d530b7a12b9a95f']
-#     },
-# }
-
-# END STORMPATH SETTING
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -174,8 +149,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+ 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+ 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
